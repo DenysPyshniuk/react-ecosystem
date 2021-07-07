44f9dc8f452ea4826155bb2c940fe42c25a42331
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-07-06 17:24:50 -0700

    Add onClick for the remove button

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./TodoListItem.css";
 
-const TodoListItem = ({ todo }) => (
+const TodoListItem = ({ todo, onRemovePressed }) => (
   <div className="todo-item-container">
     <h3 className="todo">{todo.text}</h3>
     <div className="buttons-container">
       <button className="buttons-completed">✔</button>
-      <button className="buttons-remove">❌</button>
+      <button
+        onClick={() => onRemovePressed(todo.text)}
+        className="buttons-remove"
+      >
+        ❌
+      </button>
     </div>
   </div>
 );
